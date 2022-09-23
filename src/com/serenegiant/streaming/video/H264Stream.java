6cf4db3722776c1ddc9578073db019df16ab1d5b
saki4510t@gmail.com <saki4510t@gmail.com>
2022-09-23 20:10:45 +0900

    always use MediaCodec

--- src/com/serenegiant/streaming/video/H264Stream.java
+++ src/com/serenegiant/streaming/video/H264Stream.java
@@ -19,26 +19,19 @@ package com.serenegiant.streaming.video;
 
 import android.annotation.SuppressLint;
 import android.media.MediaRecorder;
-import android.os.Bundle;
-import android.os.Environment;
 import android.service.textservice.SpellCheckerService.Session;
 import android.util.Base64;
 import android.util.Log;
-import android.view.Surface;
 
 import com.serenegiant.streaming.hw.EncoderDebugger;
 
 import net.majorkernelpanic.streaming.SessionBuilder;
-import net.majorkernelpanic.streaming.exceptions.ConfNotSupportedException;
-import net.majorkernelpanic.streaming.exceptions.StorageUnavailableException;
 import net.majorkernelpanic.streaming.mp4.MP4Config;
 import net.majorkernelpanic.streaming.rtp.H264Packetizer;
 import net.majorkernelpanic.streaming.video.VideoQuality;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
 /**
  * A class for streaming H.264 from the camera of an android device using RTP.
@@ -105,8 +98,7 @@ public class H264Stream extends VideoStream {
 	 * and determines the pps and sps. Should not be called by the UI thread.
 	 **/
 	private MP4Config testH264() throws IllegalStateException, IOException {
-		if (mMode != MODE_MEDIARECORDER_API) return testMediaCodecAPI();
-		else return testMediaRecorderAPI();
+		return testMediaCodecAPI();
 	}
 
 	@SuppressLint("NewApi")
@@ -117,128 +109,8 @@ public class H264Stream extends VideoStream {
 		} catch (Exception e) {
 			// Fallback on the old streaming method using the MediaRecorder API
 			Log.e(TAG,"Resolution not supported with the MediaCodec API, we fallback on the old streamign method.");
-			mMode = MODE_MEDIARECORDER_API;
 			return testH264();
 		}
-	}
-
-	// Should not be called by the UI thread
-	private MP4Config testMediaRecorderAPI() throws RuntimeException, IOException {
-		String key = PREF_PREFIX+"h264-mr-"+mRequestedQuality.framerate+","+mRequestedQuality.resX+","+mRequestedQuality.resY;
-
-		final Bundle settings = getSettings();
-		if (settings.containsKey(key) ) {
-			String[] s = settings.getString(key, "").split(",");
-			return new MP4Config(s[0],s[1],s[2]);
-		}
-
-		if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-			throw new StorageUnavailableException("No external storage or external storage not ready !");
-		}
-
-		final String TESTFILE = Environment.getExternalStorageDirectory().getPath()+"/spydroid-test.mp4";
-
-		Log.i(TAG,"Testing H264 support... Test file saved at: "+TESTFILE);
-
-		try {
-			File file = new File(TESTFILE);
-			file.createNewFile();
-		} catch (IOException e) {
-			throw new StorageUnavailableException(e.getMessage());
-		}
-
-		boolean previewStarted = mPreviewStarted;
-
-		// Stops the preview if needed
-		if (mPreviewStarted) {
-			mPreviewStarted = false;
-		}
-
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		try {
-			mMediaRecorder = new MediaRecorder();
-			mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-			mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-			mMediaRecorder.setVideoEncoder(mVideoEncoder);
-			mMediaRecorder.setVideoSize(mRequestedQuality.resX,mRequestedQuality.resY);
-			mMediaRecorder.setVideoFrameRate(mRequestedQuality.framerate);
-			mMediaRecorder.setVideoEncodingBitRate((int)(mRequestedQuality.bitrate*0.8));
-			mMediaRecorder.setOutputFile(TESTFILE);
-			mMediaRecorder.setMaxDuration(3000);
-
-			// We wait a little and stop recording
-			mMediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
-				public void onInfo(MediaRecorder mr, int what, int extra) {
-					Log.d(TAG,"MediaRecorder callback called !");
-					if (what==MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
-						Log.d(TAG,"MediaRecorder: MAX_DURATION_REACHED");
-					} else if (what==MediaRecorder.MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED) {
-						Log.d(TAG,"MediaRecorder: MAX_FILESIZE_REACHED");
-					} else if (what==MediaRecorder.MEDIA_RECORDER_INFO_UNKNOWN) {
-						Log.d(TAG,"MediaRecorder: INFO_UNKNOWN");
-					} else {
-						Log.d(TAG,"WTF ?");
-					}
-					mLock.release();
-				}
-			});
-
-			// Start recording
-			mMediaRecorder.prepare();
-			final Surface surface = mMediaRecorder.getSurface();
-			mSurfaceId = mSource.addSurface(surface);
-			mMediaRecorder.start();
-
-			if (mLock.tryAcquire(6,TimeUnit.SECONDS)) {
-				Log.d(TAG,"MediaRecorder callback was called :)");
-				Thread.sleep(400);
-			} else {
-				Log.d(TAG,"MediaRecorder callback was not called after 6 seconds... :(");
-			}
-		} catch (IOException e) {
-			throw new ConfNotSupportedException(e.getMessage());
-		} catch (RuntimeException e) {
-			throw new ConfNotSupportedException(e.getMessage());
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				mMediaRecorder.stop();
-			} catch (Exception e) {
-				Log.w(TAG, e);
-			}
-			mMediaRecorder.release();
-			mMediaRecorder = null;
-			if (previewStarted) {
-				// If the preview was started before the test, we try to restart it.
-				try {
-					startPreview();
-				} catch (Exception e) {
-					Log.w(TAG, e);
-				}
-			}
-		}
-
-		// Retrieve SPS & PPS & ProfileId with MP4Config
-		MP4Config config = new MP4Config(TESTFILE);
-
-		// Delete dummy video
-		File file = new File(TESTFILE);
-		if (!file.delete()) Log.e(TAG,"Temp file could not be erased");
-
-		Log.i(TAG,"H264 Test succeded...");
-
-		// Save test result
-		settings.putString(key, config.getProfileLevel()+","+config.getB64SPS()+","+config.getB64PPS());
-
-		return config;
-
 	}
 
 }
